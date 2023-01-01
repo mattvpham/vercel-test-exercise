4e4cb91cecf906f7a818ad767c4adfa34c56e581
Matt Pham <mattvpham@gmail.com>
2023-01-01 12:45:44 -0800

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Dashboard() {
+  return (
+    <main>
+      <div>Dashboard</div>
+    </main>
+  )
+}
