0eb6dd1ea4b9fe67c934638a2cb9f56f0665ef94
Matt Pham <mattvpham@gmail.com>
2023-02-06 00:43:06 -0800

    Add refresh token flow to Cognito

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions, Session } from "next-auth"
+import { JWT } from "next-auth/jwt"
 import CognitoProvider from "next-auth/providers/cognito"
 
 const clientId = process.env.COGNITO_CLIENT_ID
@@ -15,7 +16,65 @@ if (!issuer) {
   throw new Error("Missing COGNITO_ISSUER")
 }
 
-export const authOptions = {
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+    error?: string
+    refreshToken?: string
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string
+    error?: string
+  }
+}
+
+async function refreshAccessToken(token: JWT) {
+  // https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-the-refresh-token.html
+  try {
+    // InitiateAuth
+    const url = "https://cognito-idp.us-east-1.amazonaws.com/"
+
+    const response = await fetch(url, {
+      headers: {
+        "X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth",
+        "Content-Type": "application/x-amz-json-1.1",
+      },
+      body: JSON.stringify({
+        AuthFlow: "REFRESH_TOKEN_AUTH",
+        AuthParameters: {
+          REFRESH_TOKEN: token.refreshToken,
+        },
+        ClientId: process.env.COGNITO_CLIENT_ID,
+      }),
+      method: "POST",
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_at * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    }
+  } catch (error) {
+    console.log(error)
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
+}
+
+export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   providers: [
     CognitoProvider({
@@ -26,6 +85,34 @@ export const authOptions = {
       checks: ["nonce", "pkce"],
     }),
   ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // Initial sign in
+      if (account && user) {
+        const expiresAt = account.expires_at ?? 600
+        token.accessToken = account.access_token
+        token.accessTokenExpires = Date.now() + expiresAt * 1000
+        token.refreshToken = account.refresh_token
+        token.user = user
+        return token
+      }
+
+      // Return previous token if the access token has not expired yet
+      const accessTokenExpires = token.accessTokenExpires ?? Date.now()
+      if (Date.now() < accessTokenExpires) {
+        return token
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token)
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      session.error = token.error
+
+      return session
+    },
+  },
 }
 
 export default NextAuth(authOptions)
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,18 @@
 import Link from "next/link"
 import * as Sentry from "@sentry/nextjs"
 import LoginButton from "../components/login-btn"
+import { signIn, useSession } from "next-auth/react"
+import { useEffect } from "react"
 
 export default function Home() {
+  const { data: session } = useSession()
+
+  useEffect(() => {
+    if (session?.error === "RefreshAccessTokenError") {
+      signIn()
+    }
+  }, [session])
+
   return (
     <main>
       <div>A basic website</div>
